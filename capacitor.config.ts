1ba425187d2cd6c6ea22d4356c2e052a636e1ca5
Julian Suarez <julianjose2511@gmail.com>
2024-05-06 01:51:54 -0400

    primer commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'repomarcaApp',
+  appId: 'io.ionic.repomarcaApp',
+  appName: 'Repomarca App',
   webDir: 'www'
 };
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,12 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'linterna',
+    loadComponent: () => import('./linterna/linterna.page').then( m => m.LinternaPage)
+  },
 ];
